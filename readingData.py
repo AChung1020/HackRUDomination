2ed69349ebc4518aa8dc46a98ea78bc019f6d860
Jay Prasad <jbp127@scarletmail.rutgers.edu>
2023-10-07 12:33:00 -0400

    new nutrients file

--- readingData.py
+++ readingData.py
@@ -1,6 +1,24 @@
 import pandas as pd
 
-class ReadingData:
+class readingData:
 
-    def __init__(self, dataset):
-        self.dataset = dataset
+    def __init__(self, recipesDataset, ingredientsDataset, currentIngredients):
+        self.recipesDF = pd.read_csv(recipesDataset)
+        self.ingredientsDF = pd.read_csv(ingredientsDataset)
+        self.currentIngredients = currentIngredients
+
+    def getNutritonalInformation(self):
+        currentIngredientsMap = {}
+        for i in self.currentIngredients:
+            currentIngredientsMap[i] = 1
+        
+        information = []
+        
+        # Get all the recipes that contain the current ingredients
+        for name, data in self.recipesDF.iterrows():
+            currIngredient = data['ingredients']
+            if currIngredient not in currentIngredientsMap:
+                continue
+            else:
+
+
