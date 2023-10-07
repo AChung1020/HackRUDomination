780d8122555fe4bbd4afd73629d3864e3c47d028
Chris Cheng <cdc236@cornell.edu>
2023-10-07 17:12:39 -0400

    Recipe data

---
+++ vision.py
@@ -0,0 +1,62 @@
+import os
+
+from google.cloud import vision_v1
+from google.cloud.vision_v1 import types
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'spry-compound-401316-88f942825447.json'
+
+
+def localize_objects(path):
+    """Localize objects in the local image.
+
+    Args:
+    path: The path to the local file.
+    """
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
+
+    objects = client.object_localization(
+        image=image).localized_object_annotations
+
+    print(f"Number of objects found: {len(objects)}")
+    for object_ in objects:
+        print(f"\n{object_.name} (confidence: {object_.score})")
+        print("Normalized bounding polygon vertices: ")
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(f" - ({vertex.x}, {vertex.y})")
+
+
+def detect_labels(path):
+    """Detects labels in the file."""
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print("Labels:")
+
+    for label in labels:
+        print(label.description)
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(
+                response.error.message)
+        )
+
+
+path = "/Users/christophercheng/Downloads/refrigerator-full-of-healthy-food-fruit-and-vegetables-horizontal-an-opened-fridge-full-of-fresh-fruits-and-vegetables-ai-generated-free-photo.jpeg"
+localize_objects(path)
+detect_labels(path)
