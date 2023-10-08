6e66325c80f7e02fea6699dfa0e6b3eb9256cc2a
AChung1020 <achun46@emory.edu>
2023-10-08 01:07:56 -0400

    Try

--- Frontend/hackru/src/Pages/Home.js
+++ Frontend/hackru/src/Pages/Home.js
@@ -1,58 +1,54 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {useTypewriter, Cursor } from 'react-simple-typewriter';
-
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 function Home() {
     const navigate = useNavigate();
-    const [file, setFile]  = useState(null);
+    const [file, setFile] = useState(null);
+    const [message, setMessage] = useState('');  // Feedback message for user
 
-    const routeChangeLogin = () => {
-        if(file != null) {
-            let path = `/DisplayPage_Vitamins`;
-            navigate(path);
-        } else {
-            console.log("need a picture please!")
-        }
-        
-      };
-
-    
     function handleFile(event) {
-        if (event.target.files.length > 0) {
-            setFile(event.target.files[0]);
-            console.log(event.target.files[0]);
+        const selectedFile = event.target.files[0];
+        if (selectedFile && ["image/jpeg", "image/png", "image/gif"].includes(selectedFile.type)) {
+            setFile(selectedFile);
+            setMessage('');
         } else {
-            console.log("No file selected.");
+            console.log("Invalid file type selected.");
+            setMessage('Please upload a valid image file (JPEG, PNG, GIF).');
             setFile(null);
         }
     }
 
     function handleUpload(event) {
         event.preventDefault();
-        const formData = new FormData()
-        formData.append('file', file)
-        
-        if(file != null) {
-            fetch('http://localhost:5000/imageRetrieval',  //change to URL of API later
-            {  method: 'POST',
-            body: formData
-            }).then((response) => response.json())
-            .then((result) => {
-                console.log("Success", result)
-            })
-            .catch(error => {
-                console.error('error', error)
-            })
+        if (!file) {
+            setMessage('Please select an image file before submitting.');
+            return;
         }
-        
-    };
 
-    const [welcome]  = useTypewriter({
+        const formData = new FormData();
+        formData.append('file', file);
+
+        fetch('http://localhost:5000/imageRetrieval', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log("Success:", result);
+            setMessage('Image uploaded successfully!');
+            navigate('/DisplayPage_Vitamins');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            setMessage('Failed to upload the image. Please try again.');
+        });
+    }
+
+    const [welcome] = useTypewriter({
         words: ['Welcome to _____'],
         typeSpeed: 120
-    })
-
+    });
 
     return (
         <div className='Home'>
@@ -67,11 +63,10 @@ function Home() {
             
             <div className='right-half'>
                 <h1>Find out what you are missing in your diet!!! :D</h1>
+                {message && <p>{message}</p>}
                 <form onSubmit={handleUpload} method='POST' encType='multipart/form-data'>
-                    <React.Fragment>
-                        <h2>What's in your Fridge?</h2>
-                        <input type='file' onChange={handleFile}/>
-                    </React.Fragment>
+                    <h2>What's in your Fridge?</h2>
+                    <input type='file' onChange={handleFile} accept=".jpg,.jpeg,.png,.gif"/>
                     <button>Submit</button>
                 </form>
             </div>
