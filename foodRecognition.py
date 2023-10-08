8864d65f6e7ae9816f29920fd5a501387aa6396b
Jay Prasad <jbp127@scarletmail.rutgers.edu>
2023-10-07 22:23:58 -0400

    Flask backedn

--- foodRecognition.py
+++ foodRecognition.py
@@ -10,7 +10,7 @@ PAT = '4ca8cdbf393b4cea9286c349bb62acd9'
 APP_ID = 'Test'
 # Change these to make your own predictions
 WORKFLOW_ID = 'food-item-v1-recognition-workflow-2vwi4c'
-IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg'
+# IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg'
 
 ##########################################################################
 # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
@@ -20,43 +20,44 @@ from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2
 
-channel = ClarifaiChannel.get_grpc_channel()
-stub = service_pb2_grpc.V2Stub(channel)
+def determineOutcomes(imageURL):
+    IMAGE_URL = imageURL
+    channel = ClarifaiChannel.get_grpc_channel()
+    stub = service_pb2_grpc.V2Stub(channel)
 
-metadata = (('authorization', 'Key ' + PAT),)
+    metadata = (('authorization', 'Key ' + PAT),)
 
-userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID) # The userDataObject is required when using a PAT
+    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID) # The userDataObject is required when using a PAT
 
-post_workflow_results_response = stub.PostWorkflowResults(
-    service_pb2.PostWorkflowResultsRequest(
-        user_app_id=userDataObject,  
-        workflow_id=WORKFLOW_ID,
-        inputs=[
-            resources_pb2.Input(
-                data=resources_pb2.Data(
-                    image=resources_pb2.Image(
-                        url=IMAGE_URL
+    post_workflow_results_response = stub.PostWorkflowResults(
+        service_pb2.PostWorkflowResultsRequest(
+            user_app_id=userDataObject,  
+            workflow_id=WORKFLOW_ID,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        image=resources_pb2.Image(
+                            url=IMAGE_URL
+                        )
                     )
                 )
-            )
-        ]
-    ),
-    metadata=metadata
-)
-if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
-    print(post_workflow_results_response.status)
-    raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
+            ]
+        ),
+        metadata=metadata
+    )
+    if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
+        print(post_workflow_results_response.status)
+        raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
 
-# We'll get one WorkflowResult for each input we used above. Because of one input, we have here one WorkflowResult
-results = post_workflow_results_response.results[0]
+    # We'll get one WorkflowResult for each input we used above. Because of one input, we have here one WorkflowResult
+    results = post_workflow_results_response.results[0]
 
-# Each model we have in the workflow will produce one output.
-for output in results.outputs:
-    model = output.model
+    # Each model we have in the workflow will produce one output.
+    for output in results.outputs:
+        model = output.model
 
-    print("Predicted concepts for the model `%s`" % model.id)
-    for concept in output.data.concepts:
-        print("	%s %.2f" % (concept.name, concept.value))
-
-# Uncomment this line to print the full Response JSON
-#print(results)
+        # print("Predicted concepts for the model `%s`" % model.id)
+        for concept in output.data.concepts:
+            print("	%s %.2f" % (concept.name, concept.value))
+        
+    return results
--- tempName.py
+++ tempName.py
@@ -2,16 +2,55 @@ from flask import Flask, jsonify, request
 
 from flask_cors import CORS
 
+from foodRecognition import determineOutcomes
+
+import readingData
+
 app = Flask(__name__)
 CORS(app)
 
+ingredientsData = None
 
 @app.route('/data', methods={'GET'})
 def get_data():
     return jsonify({"message": "Hello from Flask!"})
 
+@app.route('/imageRetrieval', methods={'POST'})
+def getImage():
+    imageURL = request.data
+    values = determineOutcomes(imageURL)
+    # Figure out what values returns as
+    ingredientsData = readingData(values)
+    return f"Received your image!"
+
+@app.route('/valuesRetrieval', methods={'POST'})
+def getValues():
+    values = request.json['ingredients']
+    ingredientsData = readingData(values)
+    return f"Received your ingredients!"
+    
+
+@app.route('/nutritionalData', methods={'GET'})
+def get_nutritionalInformation():
+    map = jsonify(ingredientsData.determineAdditionalRecipes())
+    map["importantVitaminInfo"] = ingredientsData.getNutritionalInformation()
+    return jsonify(map)
+
+@app.route('/necessaryVitamins', methods = {'GET'})
+def get_vitamins():
+    value = jsonify(ingredientsData.determineAreasToImprove())
+
+
+@app.route('/possibleRecipes', methods = {'GET'})
+def get_recipes():
+    return jsonify(ingredientsData.populateRecipes())
+
+# @app.route('/vitaminRecipes')
+# def get_vitaminRecipes():
+#     return jsonify(ingredientsData.determineAdditionalRecipes())
+
 if __name__ == '__main__':
-    app.run(debug = True, port = 3000)
+    app.run(debug = True, port = 5000)
 
 
         
