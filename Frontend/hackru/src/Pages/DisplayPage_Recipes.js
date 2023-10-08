25d7ef68f5c8a884e8c363fb822149cf5569130e
AChung1020 <achun46@emory.edu>
2023-10-08 03:17:13 -0400

    recipe page

--- Frontend/hackru/src/Pages/DisplayPage_Recipes.js
+++ Frontend/hackru/src/Pages/DisplayPage_Recipes.js
@@ -26,13 +26,13 @@ function FoodIngredients() {
 
     return (
         <div className='background'>
-        <div className="vitamin-container">
+        <div className="recipe-container">
         <h1>Cook up a new recipe!</h1>
-            <ul className="food-list">
+            <ul className="ingredient-list">
                 {Object.entries(recipes).map(([food, ingredients]) => (
-                    <ul key={food} className="vitamin-listing">
+                    <ul key={food} className="ingredient-listing">
                         <h2>{food}</h2>
-                        <ul className="food-list">
+                        <ul className="ingredient-list">
                             {ingredients.map(ingredient => (
                                 <li key={ingredient}>{ingredient}</li>
                             ))}
