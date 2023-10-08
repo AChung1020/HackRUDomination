84a72d88a7a2e7cf5b65aa23503cf96f09acd9e4
Jay Prasad <jbp127@scarletmail.rutgers.edu>
2023-10-08 04:08:15 -0400

    recipes

--- Frontend/hackru/src/Pages/DisplayPage_Recipes.js
+++ Frontend/hackru/src/Pages/DisplayPage_Recipes.js
@@ -4,32 +4,31 @@ import { useState, useEffect } from 'react';
 
 function FoodIngredients() {
 
-      // const [data, setData] = useState();
+  const [data, setData] = useState();
 
-    // const [data, setData] = useState();
-  //const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   fetch("http://localhost:5000/data")
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setData(data);
-  //       setLoading(false);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching data:', error);
-  //       setLoading(false);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch("http://localhost:5000/possibleRecipes")
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, []);
 
-  //if (loading) return <div className="loading">Loading...</div>;
+  if (loading) return <div className="loading">Loading...</div>;
 
     return (
         <div className='background'>
         <div className="recipe-container">
         <h1>Cook up a new recipe!</h1>
             <ul className="ingredient-list">
-                {Object.entries(recipes).map(([food, ingredients]) => (
+                {Object.entries(data).map(([food, ingredients]) => (
                     <ul key={food} className="ingredient-listing">
                         <h2>{food}</h2>
                         <ul className="ingredient-list">
